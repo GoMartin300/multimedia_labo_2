a76481e125fc45cb7714ac2bc7b9d8e5afd4e818
Martijn Gevaert <martijn.gevaert@skynet.be>
2018-10-30 01:32:31 +0100

    first work done on region calculation

--- videocontroller.py
+++ videocontroller.py
@@ -13,6 +13,7 @@ class VideoController(object):
     orgtempdir = None
     targetheight = None
     targetwidth = None
+    last_region = None
 
     def __init__(self, sys_argv):
         self.targetheight = int(sys_argv[2])
@@ -30,106 +31,99 @@ class VideoController(object):
             if succes:
                 frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
                 cv2.imwrite(os.path.join(self.orgtempdir.name, str(count) + '.jpg'), frame)
-                # cv2.imwrite(os.path.join(self.saltempdir.name, str(count) + '.jpg'), codefromjakob(frame))
                 count += 1
 
-    def calculate_saliency(self):
-        count = 0
-        for picture in os.listdir(self.orgtempdir):
-            saliencymap = getSaliency(picture)
-            cv2.imwrite(os.path.join(self.saltempdir.name, saliencymap, str(count) + '.jpg'))
-            count += 1
+    # def calculate_saliency(self):
+    #     count = 0
+    #     for picture in os.listdir(self.orgtempdir):
+    #         saliencymap = getSaliency(picture)
+    #         cv2.imwrite(os.path.join(self.saltempdir.name, saliencymap, str(count) + '.jpg'))
+    #         count += 1
 
-    def calculate_binary(self, saliency_image):
+    def calculate_threshold(self, saliency_image):
         return cv2.adaptiveThreshold(saliency_image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 21, 2)
 
-    def find_region(self, sal_frame):
-        amount_h_steps = 18
-        amount_w_steps = 32
+    def find_most_salient_region(self, sal_frame):
+        # amount_h_steps = 18
+        # amount_w_steps = 32
         org_height = sal_frame.shape[0]
         org_width = sal_frame.shape[1]
-        h_step = (org_height-self.targetheight)//amount_h_steps
-        w_step = (org_width - self.targetwidth)//amount_w_steps
+        # h_step = (org_height-self.targetheight)//amount_h_steps
+        # w_step = (org_width - self.targetwidth)//amount_w_steps
+        best_value = 0b11111111111111111111111111111111
+        summed_area_matrix = np.cumsum(np.cumsum(sal_frame, axis=1, dtype='uint64'), axis=0, dtype='uint64')
         region_list = []
-        for h in range(0, amount_h_steps + 1):
-            for w in range(0, amount_w_steps + 1):
-                temp_region = Region()
-                temp_region.h1 = h * h_step
-                temp_region.h2 = temp_region.h1 + self.targetheight
-                temp_region.w1 = w * w_step
-                temp_region.w2 = temp_region.w1 + self.targetwidth
-                region_list.append(temp_region)
+        for h in range(0, org_height - self.targetheight):
+            for w in range(0, org_width - self.targetwidth):
+                A = summed_area_matrix[h][w]
+                B = summed_area_matrix[h][w + self.targetwidth]
+                C = summed_area_matrix[h + self.targetheight][w]
+                D = summed_area_matrix[h + self.targetheight][w + self.targetwidth]
+                value = D + A
+                value = value - B - C
+                if best_value > value:
+                    best_region = Region(h1=h, h2=h + self.targetheight, w1=w, w2=w + self.targetwidth)
+        return best_region
 
-        max = 0
-        max_region_number = 0
-        for region_number in range(0, len(region_list)):
-            region = region_list[region_number]
-            cut_frame = sal_frame[region.h1:region.h2+1, region.w1:region.w2+1]
-            region_saliency_sum = np.sum(cut_frame)
-            if region_saliency_sum > max:
-                max = region_saliency_sum
-                max_region_number = region_number
-        return region_list[max_region_number]
+    # def get_region_blob(self,keypoints):
+    #     im = cv2.drawKeypoints(keypoints, np.array([]), (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    #
+    #     keypLeft = int
+    #     keypRight = int
+    #     keypTop = int
+    #     keypBottom = int
+    #
+    #     #get most left,right,top,bottom pixel
+    #     leftFound = False
+    #     for x in range(0,len(im[0])):
+    #         for y in range(0, len(im)):
+    #             if im.item(y, x, 2)==255:
+    #                 keypLeft = x
+    #                 leftFound = True
+    #                 break
+    #         if leftFound:
+    #             break
+    #     rightFound = False
+    #     for x in range(len(im[0]), 0):
+    #         for y in range(0, len(im)):
+    #             if im.item(y, x, 2)==255:
+    #                 keypRight = x
+    #                 rightFound = True
+    #                 break
+    #         if rightFound:
+    #             break
+    #     topFound = False
+    #     for y in range(len(im), 0):
+    #         for x in range(0, len(im[0])):
+    #             if im.item(y, x, 2)==255:
+    #                 keypTop = y
+    #                 topFound = True
+    #                 break
+    #         if topFound:
+    #             break
+    #     bottomFound = False
+    #     for y in range(0,len(im)):
+    #         for x in range(0, len(im[0])):
+    #             if im.item(y, x, 2)==255:
+    #                 keypBottom = y
+    #                 bottomFound = True
+    #                 break
+    #         if bottomFound:
+    #             break
+    #     return Region(keypLeft,keypBottom,keypRight,keypTop)
 
-    def get_region_blob(self,keypoints):
-        im = cv2.drawKeypoints(keypoints, np.array([]), (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-
-        keypLeft = int
-        keypRight = int
-        keypTop = int
-        keypBottom = int
-
-        #get most left,right,top,bottom pixel
-        leftFound = False
-        for x in range(0,len(im[0])):
-            for y in range(0, len(im)):
-                if im.item(y, x, 2)==255:
-                    keypLeft = x
-                    leftFound = True
-                    break
-            if leftFound:
-                break
-        rightFound = False
-        for x in range(len(im[0]), 0):
-            for y in range(0, len(im)):
-                if im.item(y, x, 2)==255:
-                    keypRight = x
-                    rightFound = True
-                    break
-            if rightFound:
-                break
-        topFound = False
-        for y in range(len(im), 0):
-            for x in range(0, len(im[0])):
-                if im.item(y, x, 2)==255:
-                    keypTop = y
-                    topFound = True
-                    break
-            if topFound:
-                break
-        bottomFound = False
-        for y in range(0,len(im)):
-            for x in range(0, len(im[0])):
-                if im.item(y, x, 2)==255:
-                    keypBottom = y
-                    bottomFound = True
-                    break
-            if bottomFound:
-                break
-        return Region(keypLeft,keypBottom,keypRight,keypTop)
-
-    def blob_analysis(self, sal_frame): # https://www.learnopencv.com/blob-detection-using-opencv-python-c/
-        # set up the detector
-        detector = cv2.SimpleBlobDetector()
-        # detect blobs
-        keypoints = detector.detect(sal_frame)
-
-        # test function
-        # draw blobs as circles
-        im_with_keypoints = cv2.drawKeypoints(sal_frame, keypoints, np.array([]),(0,0,255),cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-        cv2.imshow("keyPoints", im_with_keypoints)
-        cv2.waitKey(0)
-        return self.get_region_blob(keypoints)
+    # def blob_analysis(self, sal_frame): # https://www.learnopencv.com/blob-detection-using-opencv-python-c/
+        # # set up the detector
+        # detector = cv2.SimpleBlobDetector()
+        # # detect blobs
+        # keypoints = detector.detect(sal_frame)
+        #
+        # # test function
+        # # draw blobs as circles
+        # im_with_keypoints = cv2.drawKeypoints(sal_frame, keypoints, np.array([]),(0,0,255),cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        # cv2.imshow("keyPoints", im_with_keypoints)
+        # cv2.waitKey(0)
+        # return self.get_region_blob(keypoints)
 
     def calculate_for_all(self):
         count = 0
@@ -138,11 +132,12 @@ class VideoController(object):
         # small_video = VideoWriter(os.path.join(os.getcwd(), 'multimedia_labo_3'), 'small', self.targetheight, self.targetwidth)
         for picture in os.listdir(os.fsencode(self.orgtempdir.name)):
             frame = cv2.imread(os.path.join(self.orgtempdir.name, str(count) + '.jpg'), 0)
-            frame = self.calculate_binary(frame)
+            frame = self.calculate_threshold(frame)
+            frame = self.find_most_salient_region(frame)
             # big_video.addframe(frame)
-            cv2.imwrite(os.path.join(self.saltempdir.name, str(count) + '.jpg'), frame)
-            self.blob_analysis(frame)
-            region = self.find_region(frame)
+            # cv2.imwrite(os.path.join(self.saltempdir.name, str(count) + '.jpg'), frame)
+            # self.blob_analysis(frame)
+            # region = self.find_most_salient_region(frame)
             # small_video.addframe(frame[region.h1:region.h2+1, region.w1:region.w2+1])
             count += 1
         # big_video.finish()
